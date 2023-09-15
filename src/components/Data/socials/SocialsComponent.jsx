476098e53d486e4775643601d29de4afeb97a8c8
Matias Cancina <cancinamatias@gmail.com>
2023-09-15 12:24:43 -0300

    socails link buttons

--- src/components/Data/socials/SocialsComponent.jsx
+++ src/components/Data/socials/SocialsComponent.jsx
@@ -7,19 +7,19 @@ const SocialsComponent = () => {
   return (
     <div className="col-span-1 row-span-1 lg:col-span-2 lg:row-span-2">
       <div className="grid gap-3 lg:gap-6 2xl:gap-9 w-full">
-        <div className="row-span-1 mt-3 lg:mt-0 lg:col-span-2 bg-gradient-to-b from-blue200 to-blue500 dark:from-blue800 dark:to-black rounded-3xl p-3 lg:p-8 lg:hover:scale-105 duration-150 flex justify-center items-center">
+        <a className="row-span-1 mt-3 lg:mt-0 lg:col-span-2 bg-gradient-to-b from-blue200 to-blue500 dark:from-blue800 dark:to-black rounded-3xl p-3 lg:p-8 lg:hover:scale-105 duration-150 flex justify-center items-center">
           <FormContainer />
-        </div>
-        <div className="lg:hover:scale-110 duration-150 row-span-1 col-span-1 flex justify-center items-center bg-gradient-to-b from-blue200 to-blue500 dark:from-blue800 dark:to-black  rounded-3xl p-3 lg:p-8">
-          <a href="https://www.linkedin.com/in/matiascancina/" target="_blank">
+        </a>
+        <a href="https://www.linkedin.com/in/matiascancina/" target="_blank">
+          <div className="lg:hover:scale-110 duration-150 row-span-1 col-span-1 flex justify-center items-center bg-gradient-to-b from-blue200 to-blue500 dark:from-blue800 dark:to-black  rounded-3xl p-3 lg:p-8">
             <FaLinkedin className="text-xl sm:text-3xl md:text-5xl xl:text-7xl cursor-pointer text-black dark:text-gray100" />
-          </a>
-        </div>
-        <div className="lg:hover:scale-110 duration-150 row-span-1 col-span-1 flex justify-center items-center bg-gradient-to-b from-blue200 to-blue500 dark:from-blue800 dark:to-black  rounded-3xl p-3 lg:p-8">
-          <a href="https://github.com/MatiasCancina" target="_blank">
+          </div>
+        </a>
+        <a href="https://github.com/MatiasCancina" target="_blank">
+          <div className="lg:hover:scale-110 duration-150 row-span-1 col-span-1 flex justify-center items-center bg-gradient-to-b from-blue200 to-blue500 dark:from-blue800 dark:to-black  rounded-3xl p-3 lg:p-8">
             <BsGithub className="text-xl sm:text-3xl md:text-5xl xl:text-7xl cursor-pointer text-black dark:text-gray100" />
-          </a>
-        </div>
+          </div>
+        </a>
       </div>
     </div>
   );
