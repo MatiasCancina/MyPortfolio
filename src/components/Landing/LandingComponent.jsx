a4cafefc08a17281d3fb63206d400030d20ed07d
Matias Cancina <cancinamatias@gmail.com>
2023-09-15 12:19:21 -0300

    capitalize landing

--- src/components/Landing/LandingComponent.jsx
+++ src/components/Landing/LandingComponent.jsx
@@ -21,7 +21,7 @@ const LandingComponent = () => {
         className="flex flex-col items-center justify-center text-center 2xl:top-0"
       >
         <h1 className="text-3xl sm:text-4xl md:text-5xl xl:text-6xl 2xl:text-7xl font-bold text-gray100 dark:text-black">
-          hi! I'm mati
+          Hi! I'm Mati
           <span className="text-4xl sm:text-6xl md:text-7xl xl:text-8xl 2xl:text-9xl text-blue100 dark:text-blue1000">
             .
           </span>
