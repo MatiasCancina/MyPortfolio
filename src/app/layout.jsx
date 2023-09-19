cc0b66d8a61e751f5a50511dc8e1c6a5f1d22e82
unknown <cancinamatias@gmail.com>
2023-09-18 21:21:01 -0300

    title

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
       <link rel="icon" href="./favicon.ico" />
-        <title>Matias Cancina | FullStack Dev</title>
+        <title>Matias Cancina | PORTFOLIO</title>
       </head>
       <body className={inter.className}>
         <NextUIProvider>
