1ae1fd21f394a2ad6bcf949c31a73a76593b986e
Matias Cancina <cancinamatias@gmail.com>
2023-11-21 10:52:44 -0300

    length conditional

--- src/components/Data/projects/ContainerProjects.jsx
+++ src/components/Data/projects/ContainerProjects.jsx
@@ -8,15 +8,21 @@ const MyCarousel = () => {
 
   return (
     <div className="flex items-center justify-center bg-gradient-to-b from-blue200 to-blue500 dark:from-blue800 dark:to-black rounded-3xl col-span-4 row-span-4 p-1 sm:p-6">
-      <IoIosArrowBack
-        className="lg:hover:scale-125 duration-100 text-5xl cursor-pointer text-black dark:text-gray100"
-        onClick={onClickPrevProjects}
-      />
-      {projects[position]}
-      <IoIosArrowForward
-        onClick={onClickNextProjects}
-        className="lg:hover:scale-125 duration-100 text-5xl cursor-pointer text-black dark:text-gray100"
-      />
+      {projects.length > 1 ? (
+        <>
+          <IoIosArrowBack
+            className="lg:hover:scale-125 duration-100 text-5xl cursor-pointer text-black dark:text-gray100"
+            onClick={onClickPrevProjects}
+          />
+          {projects[position]}
+          <IoIosArrowForward
+            onClick={onClickNextProjects}
+            className="lg:hover:scale-125 duration-100 text-5xl cursor-pointer text-black dark:text-gray100"
+          />
+        </>
+      ) : (
+        projects
+      )}
     </div>
   );
 };
