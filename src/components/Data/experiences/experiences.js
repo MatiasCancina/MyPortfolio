e664fb967584e3e0ddd9ac18bc494c6fa9c31eb2
Matias Cancina <cancinamatias@gmail.com>
2023-12-06 18:04:45 -0300

    hostyweb experience

--- src/components/Data/experiences/experiences.js
+++ src/components/Data/experiences/experiences.js
@@ -1,5 +1,6 @@
 import ExperiencesComponent from "./ExperiencesComponent";
 import experienceAndromeda from "../../../utils/images/experience_andromeda.png";
+import experienceHostyweb from "../../../utils/images/experience_hostyweb.png";
 
 const experiences = [
   <ExperiencesComponent
@@ -10,6 +11,15 @@ const experiences = [
     date="Oct 2023 - Nov 2023"
     fecha="Oct 2023 - Nov 2023"
     image={experienceAndromeda}
+  />,
+  <ExperiencesComponent
+    job="Full Stack Developer"
+    bussines="HostyWeb"
+    description="Development of a project that serves as an IT recruitment network, where individuals looking for job opportunities, recruiters, and companies can participate. The application is being developed using technologies such as Typescript, React, Redux Toolkit, Jest, Styled Components, and Node."
+    descripcion="Desarrollo de un proyecto que sirva como una red de reclutamiento de TI, donde puedan participar individuos en busca de oportunidades laborales, reclutadores y empresas. La aplicación se está desarrollando utilizando tecnologías como Typescript, React, Redux Toolkit, Jest, Styled Components y Node."
+    date="Nov 2023 - Dec 2023"
+    fecha="Nov 2023 - Dic 2023"
+    image={experienceHostyweb}
   />,
 ];
 
