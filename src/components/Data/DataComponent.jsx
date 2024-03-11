9b9ef0e5277fbb40ae820b47ff14b4b6577dc391
Matias Cancina <cancinamatias@gmail.com>
2024-03-11 14:41:16 -0300

    description traduction in 1 component

--- src/components/Data/DataComponent.jsx
+++ src/components/Data/DataComponent.jsx
@@ -6,20 +6,15 @@ import SocialsComponent from "./socials/SocialsComponent";
 import ContainerProyects from "./projects/ContainerProjects";
 import CertificatesContainer from "./certificates/CertificatesContainer";
 import ExperienceContainer from "./experiences/ExperienceContainer";
-import AboutMeComponentES from "./aboutMe/AboutMeComponentES";
-import { useSelector } from "react-redux";
 
 export const DataComponent = () => {
-  const language = useSelector((state) => state.language);
-
   return (
     <div
       id="data"
       className="grid rounded-3xl m-4 lg:mx-52 xl:mx-72 bg-blue100/60 dark:bg-blue1000/80 p-2 md:p-8 grid-cols-1 lg:grid-cols-4 gap-1 lg:gap-6"
     >
       <ProfilePhotoComponent />
-      {language === "en" ? <AboutMeComponent /> : <AboutMeComponentES />}
-
+      <AboutMeComponent />
       <SocialsComponent />
       <TechsComponent />
       <ExperienceContainer />
--- src/components/Data/aboutMe/AboutMeComponent.jsx
+++ src/components/Data/aboutMe/AboutMeComponent.jsx
@@ -1,34 +1,58 @@
 import React from "react";
 import { FiDownload } from "react-icons/fi";
 import { Tooltip } from "@nextui-org/react";
+import { useSelector } from "react-redux";
 
 const AboutMeComponent = () => {
+  const language = useSelector((state) => state.language);
+
   return (
     <div className="bg-blue300 dark:bg-blue800 rounded-3xl col-span-4 xl:col-span-3 p-8">
       <div className="flex items-center">
         <h1 className="text-base sm:text-lg md:text-xl xl:text-2xl font-semibold text-black dark:text-gray100">
           Matías Cancina
         </h1>
-        <Tooltip content={"Download Resume (English)"} color="primary">
-          <a
-            download href="/assets/Matias Cancina Resume.pdf"
-            className="outline-none"
-          >
-            <FiDownload className="text:xl sm:text-2xl text-black dark:text-gray100 cursor-pointer lg:hover:scale-125 duration-200 mx-4" />
-          </a>
-        </Tooltip>
+        {language === "en" ? (
+          <Tooltip content={"Download Resume (English)"} color="primary">
+            <a
+              download
+              href="/assets/Matias Cancina Resume.pdf"
+              className="outline-none"
+            >
+              <FiDownload className="text:xl sm:text-2xl text-black dark:text-gray100 cursor-pointer lg:hover:scale-125 duration-200 mx-4" />
+            </a>
+          </Tooltip>
+        ) : (
+          <Tooltip content={"Descargar CV (Español)"} color="primary">
+            <a
+              className="outline-none"
+              download
+              href="/assets/CV Matias Cancina.pdf"
+            >
+              <FiDownload className="text:xl sm:text-2xl text-black dark:text-gray100 cursor-pointer lg:hover:scale-125 duration-200 mx-4" />
+            </a>
+          </Tooltip>
+        )}
       </div>
-      <p className="font-medium text-xs sm:text-base xl:text-lg p-1 sm:p-4 text-black dark:text-gray100">
-        I am an Argentinian
-        <strong className="mx-1">Full Stack Developer</strong>
-        with experience in
-        <strong className="mx-1">Front-End</strong>
-        and
-        <strong className="mx-1">Back-End</strong>
-        since 2022. My passion is to create exceptional digital experiences,
-        paying attention to every detail. I stand out for staying up to date
-        with the latest development trends and tools.
-      </p>
+      {language === "en" ? (
+        <p className="font-medium text-xs sm:text-base xl:text-lg p-1 sm:p-4 text-black dark:text-gray100">
+          I am an Argentinian
+          <strong className="mx-1">Front-End Developer</strong>
+          who has been making progress in the programming environment since
+          2022. My passion is to create exceptional digital experiences, paying
+          attention to every detail. I stand out for staying up to date with the
+          latest development trends and tools.
+        </p>
+      ) : (
+        <p className="font-medium text-xs sm:text-base xl:text-lg p-1 sm:p-4 text-black dark:text-gray100">
+          Soy un
+          <strong className="mx-1">Desarrollador Front-End</strong>
+          argentino que viene progresando en el entorno de la programacion desde
+          2022. Mi pasión es crear experiencias digitales excepcionales,
+          cuidando cada detalle. Me destaco por mantenerme al día con las
+          últimas tendencias y herramientas de desarrollo.
+        </p>
+      )}
     </div>
   );
 };
