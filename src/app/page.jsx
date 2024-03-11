82517d8f6fea2bab42f79ca5af9e9788c399cc97
Matias Cancina <cancinamatias@gmail.com>
2024-03-11 14:27:37 -0300

    landing traduction in 2 components

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,30 +1,17 @@
 "use client";
 import { DataComponent } from "@/components/Data/DataComponent";
-import DataComponentES from "@/components/Data/DataComponentES";
 import FooterComponent from "@/components/Footer/FooterComponent";
 import LandingComponent from "@/components/Landing/LandingComponent";
-import LandingComponentES from "@/components/Landing/LandingComponentES";
 import { useSelector } from "react-redux";
 
 export default function Home() {
-  const language = useSelector((state) => state.language);
-
   return (
     <main>
       <div
         className={`flex w-full min-h-screen relative items-center justify-center bg-[url('../utils/images/photo3dlight2.jpg')] dark:bg-[url('../utils/images/photo3ddark1.jpg')] flex-col 2xl:p-24 bg-no-repeat bg-cover transition-all ease-in-out duration-500`}
       >
-        {language === "en" ? (
-          <>
-            <LandingComponent />
-            <DataComponent />
-          </>
-        ) : (
-          <>
-            <LandingComponentES />
-            <DataComponentES />
-          </>
-        )}
+        <LandingComponent />
+        <DataComponent />
       </div>
       <FooterComponent />
     </main>
--- src/components/Landing/LandingComponent.jsx
+++ src/components/Landing/LandingComponent.jsx
@@ -3,8 +3,11 @@ import SwitchTheme from "./switches/SwitchTheme";
 import LanguageSwitcher from "./switches/LanguageSwitcher";
 import LinkBtn from "./linkBtn.jsx/LinkBtn";
 import { motion } from "framer-motion";
+import { useSelector } from "react-redux";
 
 const LandingComponent = () => {
+  const language = useSelector((state) => state.language);
+
   return (
     <div className="flex flex-col items-center min-h-screen justify-center">
       <div className="flex absolute top-10 sm:top-20 xl:top-10 items-center gap-5 sm:gap-10">
@@ -20,15 +23,31 @@ const LandingComponent = () => {
         }}
         className="flex flex-col items-center justify-center text-center 2xl:top-0"
       >
-        <h1 className="text-3xl sm:text-4xl md:text-5xl xl:text-6xl 2xl:text-7xl font-bold text-gray100 dark:text-black">
-          Hi! I'm Mati
-          <span className="text-4xl sm:text-6xl md:text-7xl xl:text-8xl 2xl:text-9xl text-blue100 dark:text-blue1000">
-            .
-          </span>
-        </h1>
-        <span className="text-sm sm:text-base md:text-lg xl:text-xl 2xl:text-2xl font-black mt-3 sm:mt-4 xl:mt-5 2xl:mt-6 text-blue100 dark:text-blue1000">
-          Full Stack Developer
-        </span>
+        {language === "en" ? (
+          <>
+            <h1 className="text-3xl sm:text-4xl md:text-5xl xl:text-6xl 2xl:text-7xl font-bold text-gray100 dark:text-black">
+              Hi! I'm Mati
+              <span className="text-4xl sm:text-6xl md:text-7xl xl:text-8xl 2xl:text-9xl text-blue100 dark:text-blue1000">
+                .
+              </span>
+            </h1>
+            <span className="text-sm sm:text-base md:text-lg xl:text-xl 2xl:text-2xl font-black mt-3 sm:mt-4 xl:mt-5 2xl:mt-6 text-blue100 dark:text-blue1000">
+              Front End Developer
+            </span>
+          </>
+        ) : (
+          <>
+            <h1 className="text-3xl sm:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl font-bold text-gray100 dark:text-black">
+              ¡Hola! Soy Mati
+              <span className="text-4xl sm:text-6xl md:text-7xl xl:text-8xl 2xl:text-9xl text-blue100 dark:text-blue1000">
+                .
+              </span>
+            </h1>
+            <span className="text-sm sm:text-base md:text-lg xl:text-xl 2xl:text-2xl font-black mt-3 sm:mt-4 xl:mt-5 2xl:mt-6 text-blue100 dark:text-blue1000">
+              Desarrollador Front End
+            </span>
+          </>
+        )}
       </motion.div>
       <LinkBtn />
     </div>
